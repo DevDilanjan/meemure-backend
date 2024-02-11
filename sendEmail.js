634d3998fd6a713d9fcd84bcc04ae262e9763530
DevDilanjan <136694720+DevDilanjan@users.noreply.github.com>
2024-02-11 20:26:52 +0530

    done

---
+++ sendEmail.js
@@ -0,0 +1,41 @@
+const nodemailer = require("nodemailer");
+
+exports.sendEmail = async (data) => {
+  const { name, email, referenceNumber } = data;
+
+  const serviceEmail = process.env.SERVICE_EMAIL;
+  const servicePassword = process.env.SERVICE_PASSWORD;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: serviceEmail,
+      pass: servicePassword,
+    },
+  });
+
+  const confirmationMsg = {
+    to: email,
+    from: serviceEmail,
+    subject: "Booking Confirmation",
+    text: `Dear ${name},\n\nYour booking reference number ${referenceNumber}. 
+    Thank you!`,
+  };
+
+  const detailsMsg = {
+    to: serviceEmail,
+    from: email,
+    subject: `New Booking, reference number ${referenceNumber}`,
+    text: `Booking details for ${name}:\n\n${JSON.stringify(data, null, 2)}`,
+  };
+
+  try {
+    await transporter.sendMail(confirmationMsg);
+
+    await transporter.sendMail(detailsMsg);
+    return true;
+  } catch (error) {
+    console.log("Error sending Email!", error);
+    return false;
+  }
+};
